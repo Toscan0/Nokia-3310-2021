7ad36f95d8be89d74e9a6bc488e8ec2f0ef73993
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-03 23:19:10 +0000

    timer implemented

---
+++ Game/Assets/Scripts/HighScore/Timer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    private Text timerText;
+
+    private float startTime;
+    private static float currentTime = 0;
+
+    void Start()
+    {
+        startTime = Time.time;
+        SetTimerText();
+    }
+
+    void Update()
+    {
+        currentTime = Time.time - startTime;
+
+        SetTimerText();
+    }
+
+    private void SetTimerText()
+    {
+        timerText.text = GetMinutes(currentTime)
+        + ":" + GetSecondsToDisplay(currentTime) + ":"
+        + GetMilliseconds(currentTime);
+    }
+
+    private string GetMinutes(float seconds)
+    {
+        return ((int)seconds / 60).ToString();
+    }
+
+    private string GetSecondsToDisplay(float seconds)
+    {
+        return (seconds % 60).ToString("f0");
+    }
+
+    private string GetMilliseconds(float seconds)
+    {
+        return ((seconds * 1000) % 1000).ToString("f0");
+    }
+}
