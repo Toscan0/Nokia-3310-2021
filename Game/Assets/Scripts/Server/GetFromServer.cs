b7da4ca1067b766c63c7043262de64374525b1b6
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-04 21:40:04 +0000

    Working on Server connection

---
+++ Game/Assets/Scripts/Server/GetFromServer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GetFromServer : MonoBehaviour
+{
+    private const string GET_HIGHSCORE_URL = "" +
+        "http://web.tecnico.ulisboa.pt/~ist181633/MemoryGame/Server/Get_HighScore.php";
+
+    public string ServerData { get; private set; }
+
+    public IEnumerator GetHighScore()
+    {
+        string url = GET_HIGHSCORE_URL;
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(GET_HIGHSCORE_URL))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError)
+            {
+                Debug.LogError("UnityWebRequest get error:" + webRequest.error);
+            }
+            else
+            {
+                ServerData = ParseServerData(webRequest.downloadHandler.text);
+            }
+        }
+    }
+    
+    private string ParseServerData(string data)
+    {
+        Debug.Log(data);
+
+        return data;
+    }
+}
---
+++ Game/Assets/Scripts/Server/PostOnServer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PostOnServer : MonoBehaviour
+{
+    private const string POST_HIGHSCORE_URL = "" + 
+        "http://web.tecnico.ulisboa.pt/~ist181633/MemoryGame/Server/Post_HighScore.php";
+
+    public IEnumerator PostHighScore(string playerName, float time)
+    {
+        List<IMultipartFormSection> wwwForm = new List<IMultipartFormSection>();
+
+        string highScoreToSend = ParseDataToSend(playerName, time);
+
+        wwwForm.Add(new MultipartFormDataSection("_content", highScoreToSend));
+
+        UnityWebRequest www = UnityWebRequest.Post(POST_HIGHSCORE_URL, wwwForm);
+
+        //w8 for answer
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.LogError("UnityWebRequest post error: " + www.error);
+        }
+    }
+
+    private string ParseDataToSend(string playerName, float time)
+    {
+        Debug.Log(playerName + " " + time);
+
+        return null;
+    }
+}
