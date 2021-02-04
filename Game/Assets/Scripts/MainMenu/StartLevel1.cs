bc0019800cade3faa93acb857ab4809b0d17d195
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-04 18:01:19 +0000

    buttons are not allowed - removed

---
+++ Game/Assets/Scripts/MainMenu/StartLevel1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartLevel1 : MonoBehaviour
+{
+    [SerializeField]
+    private LoadSceneManager loadSceneManager;
+
+    private void Start()
+    {
+        Invoke("PlayGame", 1f);
+    }
+
+    private void PlayGame()
+    {
+        loadSceneManager.LoadNextScene();
+    }
+}
